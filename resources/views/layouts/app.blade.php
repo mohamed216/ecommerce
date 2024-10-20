c3ae0965d20a33c93c39bd116e88bbf6d19efcad
mohamed216 <mhmed216@gmail.com>
2024-10-20 17:53:48 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
     <link rel="stylesheet" href="{{asset('assets/css/plugins/swiper.min.css')}}" type="text/css" />
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" type="text/css" />
     <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/sweetalert.min.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
           integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
           crossorigin="anonymous" referrerpolicy="no-referrer">
@@ -270,16 +271,19 @@
         </a>
 
         <div class="logo">
-            <a href="index.html">
+            <a href="{{route('home.index')}}">
                 <img src="{{asset('assets/images/logo.png')}}" alt="Uomo" class="logo__image d-block" />
             </a>
         </div>
 
-        <a href="#" class="header-tools__item header-tools__cart js-open-aside" data-aside="cartDrawer">
+        <a href="{{route('cart.index')}}" class="header-tools__item header-tools__cart">
             <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <use href="#icon_cart" />
             </svg>
-            <span class="cart-amount d-block position-absolute js-cart-items-count">3</span>
+
+            @if(Cart::instance("cart")->content()->count()>0)
+                <span class="cart-amount d-block position-absolute js-cart-items-count">{{Cart::instance("cart")->content()->count()}}</span>
+            @endif
         </a>
     </div>
 
@@ -309,13 +313,13 @@
             <div class="overflow-hidden">
                 <ul class="navigation__list list-unstyled position-relative">
                     <li class="navigation__item">
-                        <a href="index.html" class="navigation__link">Home</a>
+                        <a href="{{route('home.index')}}" class="navigation__link">Home</a>
                     </li>
                     <li class="navigation__item">
                         <a href="shop.html" class="navigation__link">Shop</a>
                     </li>
                     <li class="navigation__item">
-                        <a href="cart.html" class="navigation__link">Cart</a>
+                        <a href="{{route('cart.index')}}" class="navigation__link">Cart</a>
                     </li>
                     <li class="navigation__item">
                         <a href="about.html" class="navigation__link">About</a>
@@ -390,7 +394,7 @@
     <div class="container">
         <div class="header-desk header-desk_type_1">
             <div class="logo">
-                <a href="index.html">
+                <a href="{{route('home.index')}}">
                     <img src="{{asset('assets/images/logo.png')}}" alt="Uomo" class="logo__image d-block" />
                 </a>
             </div>
@@ -398,13 +402,13 @@
             <nav class="navigation">
                 <ul class="navigation__list list-unstyled d-flex">
                     <li class="navigation__item">
-                        <a href="index.html" class="navigation__link">Home</a>
+                        <a href="{{route('home.index')}}" class="navigation__link">Home</a>
                     </li>
                     <li class="navigation__item">
-                        <a href="shop.html" class="navigation__link">Shop</a>
+                        <a href="{{route('shop.index')}}" class="navigation__link">Shop</a>
                     </li>
                     <li class="navigation__item">
-                        <a href="cart.html" class="navigation__link">Cart</a>
+                        <a href="{{route('cart.index')}}" class="navigation__link">Cart</a>
                     </li>
                     <li class="navigation__item">
                         <a href="about.html" class="navigation__link">About</a>
@@ -462,27 +466,43 @@
                     </div>
                 </div>
 
-                <div class="header-tools__item hover-container">
-                    <a href="login.html" class="header-tools__item">
-                        <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <use href="#icon_user" />
-                        </svg>
-                    </a>
-                </div>
+                @guest
+                    <div class="header-tools__item hover-container">
+                        <a class="header-tools__item" href="{{route('login')}}">
+                            <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <use href="#icon_user" />
+                            </svg>
+                        </a>
+                    </div>
+                @else
+                    <div class="header-tools__item hover-container">
+                        <a class="header-tools__item" href="{{ Auth::user()->utype=='ADM' ? route('admin.index') : route('user.account.dashboard')}}">
+                            <span class="pr-6px">{{Auth::user()->name}}</span>
+                            <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <use href="#icon_user" />
+                            </svg>
+                        </a>
+                    </div>
+                @endguest
 
-                <a href="wishlist.html" class="header-tools__item">
+
+                <a href="{{route('wishlist.index')}}" class="header-tools__item header-tools__wishlist header-tools__cart" >
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_heart" />
                     </svg>
+                    @if(Cart::instance("wishlist")->content()->count()>0)
+                        <span class="cart-amount d-block position-absolute js-cart-items-count">{{Cart::instance("wishlist")->content()->count()}}</span>
+                    @endif
                 </a>
 
-                <a href="cart.html" class="header-tools__item header-tools__cart">
+                <a href="{{route('cart.index')}}" class="header-tools__item header-tools__cart">
                     <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_cart" />
                     </svg>
-                    <span class="cart-amount d-block position-absolute js-cart-items-count">3</span>
+                    @if(Cart::instance('cart')->content()->count()>0)
+                    <span class="cart-amount d-block position-absolute js-cart-items-count">{{Cart::instance('cart')->content()->count()}}</span>
+                    @endif
                 </a>
             </div>
         </div>
@@ -497,7 +517,7 @@
         <div class="row row-cols-lg-5 row-cols-2">
             <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
                 <div class="logo">
-                    <a href="index.html">
+                    <a href="{{route('home.index')}}">
                         <img src="{{asset('assets/images/logo.png')}}" alt="SurfsideMedia" class="logo__image d-block" />
                     </a>
                 </div>
@@ -613,7 +633,7 @@
 <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
     <div class="row text-center">
         <div class="col-4">
-            <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+            <a href="{{route('home.index')}}" class="footer-mobile__link d-flex flex-column align-items-center">
                 <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                      xmlns="http://www.w3.org/2000/svg">
                     <use href="#icon_home" />
@@ -623,7 +643,7 @@
         </div>
 
         <div class="col-4">
-            <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+            <a href="{{route('home.index')}}" class="footer-mobile__link d-flex flex-column align-items-center">
                 <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                      xmlns="http://www.w3.org/2000/svg">
                     <use href="#icon_hanger" />
@@ -633,7 +653,7 @@
         </div>
 
         <div class="col-4">
-            <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+            <a href="{{route('home.index')}}" class="footer-mobile__link d-flex flex-column align-items-center">
                 <div class="position-relative">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
@@ -654,6 +674,7 @@
 <script src="{{asset('assets/js/plugins/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('assets/js/plugins/bootstrap-slider.min.js')}}"></script>
 <script src="{{asset('assets/js/plugins/swiper.min.js')}}"></script>
+<script src="{{asset('js/sweetalert.min.js')}}"></script>
 <script src="{{asset('assets/js/plugins/countdown.js')}}"></script>
 <script src="{{asset('assets/js/theme.js')}}"></script>
 @stack("scripts")
